482a6b4b323d059838b6a5ef4ea77d85bd023ecd
Mark Rodgers <markrodgers.dev@gmail.com>
2025-01-17 19:46:22 +0000

    feat: Add personal section inputs

--- src/components/CurriculumVitae.jsx
+++ src/components/CurriculumVitae.jsx
@@ -1,9 +1,23 @@
 import Personal from "./Personal";
+import { useState } from "react";
 
 function CurriculumVitae() {
+  const [personal, setPersonal] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    github: "",
+  });
+
   return (
     <>
-      <Personal />
+      <Personal personal={personal} updatePersonal={setPersonal} />
+      {/* <div>
+        <p>{personal.name}</p>
+        <p>{personal.email}</p>
+        <p>{personal.phone}</p>
+        <p>{personal.github}</p>
+      </div> */}
     </>
   );
 }
--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -1,25 +1,79 @@
-function Personal() {
-  return <></>;
+/* 
+todo:
+  bonus- svg of person?
+  bonus- dropdown?
+
+  correct way to handle the change?
+*/
+
+function Personal({ personal, updatePersonal }) {
+  function handleNameChange(e) {
+    updatePersonal({ ...personal, name: e.target.value });
+  }
+
+  function handleEmailChange(e) {
+    updatePersonal({ ...personal, email: e.target.value });
+  }
+
+  function handlePhoneChange(e) {
+    updatePersonal({ ...personal, phone: e.target.value });
+  }
+
+  function handleGithubChange(e) {
+    updatePersonal({ ...personal, github: e.target.value });
+  }
+
+  return (
+    <>
+      <h2>Personal Details</h2>
+
+      <form action="#" id="personal">
+        <label htmlFor="name">
+          Full Name:
+          <input
+            id="name"
+            name="name"
+            type="text"
+            value={personal.name}
+            onChange={handleNameChange}
+          />
+        </label>
+
+        <label htmlFor="email">
+          Email:
+          <input
+            id="email"
+            name="email"
+            type="email"
+            value={personal.email}
+            onChange={handleEmailChange}
+          />
+        </label>
+
+        <label htmlFor="phone">
+          Phone:
+          <input
+            id="phone"
+            name="phone"
+            type="phone"
+            value={personal.phone}
+            onChange={handlePhoneChange}
+          />
+        </label>
+
+        <label htmlFor="github">
+          Github:
+          <input
+            id="github"
+            name="github"
+            type="text"
+            value={personal.github}
+            onChange={handleGithubChange}
+          />
+        </label>
+      </form>
+    </>
+  );
 }
 
 export default Personal;
-
-/* 
-bonus: svg of person?
-
-h2 - personal
-
-name - lable/h3
-input text
-
-email - lable/h3
-input email
-
-phone - lable/h3
-input phone
-
-github - lable/h3 
-input text
-
-bonus: dropdown?
-*/
